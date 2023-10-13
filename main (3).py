9a653974fb8a586194ced79ca008963270f60a38
paramesh2006 <144789995+paramesh2006@users.noreply.github.com>
2023-10-13 20:51:35 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,18 @@
+...implement a class called bank account that represents a bank account.the class should have activated 
+attributes  for accountnumber, account holdername , account balance.include methods to
+deposit money, withdrawal moneny,and display the account balance.insure that the  account balance.
+cannot be accessed  directly from  outside the class.write a program to create an instance of  the
+bankaccount class  and test the deposit  and withdrawal  functionality...
+
+
+class bankaccount:
+
+  def__fact__(self, account_number, account_holder_name,initial_balance=0.0);
+ self,__ account_number=account _number
+  self,__account_holder_name=account _holder_name
+self,__account_balance=initial balance
+
+def__deposite( self, amount ):
+   if amount>0:
+     self,__ account _balance== amount
+print (" deposited")
